426eb15e4ed3df443b967dbf3195ff712d9e57f0
Vitalii <vitaly.n1@gmail.com>
2018-10-22 16:56:32 -0700

    Templates for structures

---
+++ array.py
@@ -0,0 +1,15 @@
+
+def bubble_sort(array):
+    pass
+
+
+def insert_sort(array):
+    pass
+
+
+def quick_sort(array):
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ tree.py
@@ -0,0 +1,20 @@
+class Node(object):
+    left = None
+    right = None
+
+    def __init__(self, data=None):
+        self.value = data
+
+
+class Tree(object):
+    length = 0
+
+    def __init__(self, data=None):
+        self.root = None(data)
+
+    def insert(self, data):
+        new_node = Node(data)
+
+
+if __name__ == "__main__":
+    pass
