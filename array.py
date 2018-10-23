daec11d593d1c1ecae0abb694a12b939d09d7fed
Vitalii <vitaly.n1@gmail.com>
2018-10-22 21:27:14 -0700

    Search and bst methods implementation

--- array.py
+++ array.py
@@ -1,10 +1,41 @@
 
 def bubble_sort(array):
-    pass
+    len_arr = len(array)-1
+    while len_arr > 0:
+        i = 0
+        while i+1 <= len_arr:
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+            i += 1
+        len_arr -= 1
+    return array
 
 
 def insert_sort(array):
-    pass
+    for i in range(1, len(array)):
+        insertion = array[i]
+        j = i-1
+        while array[j] > insertion and j >= 0:
+            array[j+1] = array[j]
+            j = j - 1
+        array[j+1] = insertion
+    return array
+
+
+def selection_sort(array):
+    len_arr = len(array)-1
+    for i in range(len_arr):
+        curr_min = array[i]
+        curr_pos = i
+        j = i+1
+        while j <= len_arr:
+            if array[j] < curr_min:
+                curr_min = array[j]
+                curr_pos = j
+            j += 1
+        if curr_pos != i:
+            array[i], array[curr_pos] = array[curr_pos], array[i]
+    return array
 
 
 def quick_sort(array):
@@ -12,4 +43,6 @@ def quick_sort(array):
 
 
 if __name__ == "__main__":
-    pass
+    print(bubble_sort([7, 5, 4, 3, 2, 1, 0]))
+    print(selection_sort([7, 5, 4, 3, 2, 1, 0]))
+    print(insert_sort([1, 5, 2, 3, 4, 6, 0]))
--- tree.py
+++ tree.py
@@ -6,15 +6,68 @@ class Node(object):
         self.value = data
 
 
-class Tree(object):
+class BsTree(object):
     length = 0
+    root = None
 
     def __init__(self, data=None):
-        self.root = None(data)
+        self.root = Node(data)
 
-    def insert(self, data):
-        new_node = Node(data)
+    def insert(self, value, curr_node=None):
+        new_node = Node(value)
+        if not curr_node:
+            curr_node = self.root
+        if curr_node.value:
+            if value < curr_node.value:
+                if curr_node.left:
+                    self.insert(value, curr_node.left)
+                else:
+                    curr_node.left = new_node
+            elif value > curr_node.value:
+                if curr_node.right:
+                    self.insert(value, curr_node.right)
+                else:
+                    curr_node.right = new_node
+            else:
+                print("Insertion error")
+
+    def search(self, value, curr_node=None, path=None):
+        if not path:
+            path = []
+        if not curr_node:
+            curr_node = self.root
+        if value == curr_node.value:
+            path.append(curr_node.value)
+            return curr_node, path
+        elif curr_node.left and value < curr_node.value:
+            path.append(curr_node.value)
+            return self.search(value, curr_node.left, path)
+        elif curr_node.right and value > curr_node.value:
+            path.append(curr_node.value)
+            return self.search(value, curr_node.right, path)
+        else:
+            return None, None
+
+    def delete(self, value):
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    new_tree = BsTree(10)
+    # new_tree.root.left = Node(4)
+    # new_tree.root.right = Node(17)
+    # new_tree.root.left.left = Node(2)
+    # new_tree.root.left.right = Node(5)
+    # new_tree.root.right.left = Node(15)
+    # new_tree.root.right.right = Node(20)
+    new_tree.insert(4)
+    new_tree.insert(17)
+    new_tree.insert(2)
+    new_tree.insert(5)
+    new_tree.insert(15)
+    new_tree.insert(20)
+    new_tree.insert(4)
+
+    node, arr = new_tree.search(2)
+    print(arr)
+
